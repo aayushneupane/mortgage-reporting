266efe27031931e50d2bc5648cfffed0310d4b18
Aayush Neupane <aayushalex@gmail.com>
2021-08-08 11:05:41 -0500

    full client functionality before applying syling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,54 +1,51 @@
 <template>
-  <header>
-    <router-link to="/">
-    <h1>
-      Mortgage<span>Accounts</span>
-    </h1>
-    </router-link>
-  </header>
-  <main>
-
-  </main>
-  <router-view />
+  <div>
+    <header>
+      <router-link to="/">
+        <h1>Mortgage<span>Accounts</span></h1>
+      </router-link>
+    </header>
+    <main><router-view /></main>
+  </div>
 </template>
 
 <style lang="scss">
- * {
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
-   font-family: 'Fira Sans', sans-serif;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
 
-   &::selection {
-     background: transparentize($color: #42B883, $amount: 0.5)
-   }
- }
+  &::selection {
+    background: transparentize($color: #42b883, $amount: 0.5);
+  }
+}
 
- body {
-   background-color: #82a1c3;
- }
+body {
+  // background-color: #f5fbff;
+  background-color: #b1bfd8;
+  background-image: linear-gradient(315deg, #b1bfd8 0%, #6782b4 74%);
+}
 
- a {
-   text-decoration: none;
- }
+a {
+  text-decoration: none;
+}
 
- header {
-   display: flex;
-   align-items: center;
-   justify-content: center;
-   padding: 10px 16px;
-   background-color: #2c3d4e;
-   box-shadow: 0px 0px 6px rgba(0,0,0,0.1);
- }
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+}
 
- h1 {
-   color: #fff;
-   font-size: 28px;
+h1 {
+  color: #fff;
+  font-size: 28px;
 
-   span {
-     color: #42B883;
-   }
- }
-
-
+  span {
+    color: #42b883;
+  }
+}
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,26 +1,23 @@
 <template>
-  <div class="home">
-    <div class="featured-card">
-      <router-link to="/account/9c236bbf-7d55-4393-8f56-731009a13538">
-        <img
-          src="https://i.pravatar.cc/280"
-          alt="This person"
-          class="featured-image"
-        />
-        <div class="detail">
-          <h3>Verna</h3>
-          <p>
-            ariatur ad ea esse. Cillum ex laboris quis culpa culpa ex
-            adipisicing esse pariatur non nisi
-          </p>
-        </div>
-      </router-link>
-    </div>
+  <div class="home container">
     <form @submit.prevent="searchAccounts()" class="search-box">
       <input type="text" placeholder="Enter account detail" v-model="search" />
       <input type="submit" value="Search" />
     </form>
-    <div class="account-list">ACCOUNTS</div>
+    <div class="accounts-list">
+      <div class="account" v-for="account in accounts" :key="account">
+        <router-link :to="'/account/' + account.id" class="account-link">
+          <div class="product-image">
+            <img :src="account.picture" alt="profile picture" />
+            <div class="type">Type</div>
+          </div>
+          <div class="detail">
+            <h3>{{ account.name_last }}, {{ account.name_first }}</h3>
+            <p>{{ account.comments }}</p>
+          </div>
+        </router-link>
+      </div>
+    </div>
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
   </div>
 </template>
@@ -32,11 +29,23 @@ import { ref } from "vue";
 export default {
   setup() {
     const search = ref("");
-    const accounts = ref([]);
+    const accounts = ref();
+    const a = ref();
 
     const searchAccounts = () => {
       if (search.value != "") {
         console.log("search value:", search.value);
+        fetch(`http://localhost:8080/api/account/?firstName=${search.value}`)
+          .then((response) => response.json())
+          .then((data) => {
+            accounts.value = data;
+            a.value = data[0];
+            // search.value = "";
+            console.log(accounts.value);
+          })
+          .catch((err) => {
+            console.log("Not found. ", err.message);
+          });
       }
     };
 
@@ -44,6 +53,7 @@ export default {
       search,
       accounts,
       searchAccounts,
+      a,
     };
   },
 };
@@ -129,5 +139,176 @@ export default {
       }
     }
   }
+
+  .accounts-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .account {
+      max-width: 300px;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .account-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: #fff;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          p {
+            color: #fff;
+          }
+        }
+
+        h3 {
+          color: #fff;
+          font-weight: 600;
+          font-size: 18px;
+
+          span {
+            color: #42b883;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+  background-color: #141625;
+}
+
+button,
+.button {
+  cursor: pointer;
+  padding: 16px 24px;
+  border-radius: 30px;
+  border: none;
+  font-size: 12px;
+  margin-right: 8px;
+  color: #fff;
+}
+
+.dark-purple {
+  background-color: #252945;
+}
+
+.red {
+  background-color: #ec5757;
+}
+
+.purple {
+  background-color: #7c5dfa;
+}
+
+.green {
+  background-color: #33d69f;
+}
+
+.orange {
+  background-color: #ff8f00;
+}
+
+// utility classes
+
+.flex {
+  display: flex;
+}
+
+.flex-column {
+  flex-direction: column;
+}
+
+.container {
+  width: 100%;
+  padding: 40px 10px;
+  max-width: 850px;
+  margin: 0 auto;
+
+  @media (min-width: 900px) {
+    padding-top: 72px;
+  }
+}
+
+.nav-link {
+  text-decoration: none;
+  color: initial;
+}
+
+// Status Button Styling
+
+.status-button {
+  &::before {
+    content: "";
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    margin-right: 8px;
+  }
+  font-size: 12px;
+  margin-right: 30px;
+  align-items: center;
+  padding: 8px 30px;
+  border-radius: 10px;
+}
+
+.paid {
+  &::before {
+    background-color: #33d69f;
+  }
+  color: #33d69f;
+  background-color: rgba(51, 214, 160, 0.1);
+}
+
+.pending {
+  &::before {
+    background-color: #ff8f00;
+  }
+  color: #ff8f00;
+  background-color: rgba(255, 145, 0, 0.1);
+}
+
+.draft {
+  &::before {
+    background-color: #dfe3fa;
+  }
+  color: #dfe3fa;
+  background-color: rgba(223, 227, 250, 0.1);
 }
 </style>
